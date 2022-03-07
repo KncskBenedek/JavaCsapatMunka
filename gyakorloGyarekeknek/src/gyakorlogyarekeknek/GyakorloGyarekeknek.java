f5ba6a32ca41ad62e8db4973cce7e8900ec70656
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-07 21:53:41 +0100

    kezdeti állapot

--- gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
+++ gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
@@ -1,11 +1,41 @@
 package gyakorlogyarekeknek;
 
+import java.util.Scanner;
+
 
 public class GyakorloGyarekeknek {
     
     
     public static void main(String[] args) {
-       System.out.println("Szia");
+        bekerFeladatTipus();
+        int valasz = feladatOsszeAllit();
+        kiir();
+        bekerValasz();
+        
+        
+      
+      
     }
+
+    private static int feladatOsszeAllit() {
+           
+    }
+
+    private static void bekerFeladatTipus() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static int feladat() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void kiir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static void bekerValasz() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
     
 }
