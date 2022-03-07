0ee1dd25ca85613ed19864c93dd4b8dfe2a9d9ae
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-07 22:33:27 +0100

    metódusok

--- gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
+++ gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
@@ -1,28 +1,27 @@
 package gyakorlogyarekeknek;
 
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class GyakorloGyarekeknek {
     
-    
-    public static void main(String[] args) {
-        bekerFeladatTipus();
-        int valasz = feladatOsszeAllit();
-        kiir();
-        bekerValasz();
-        
-        
-      
-      
+    static int[] rosszMegoldasok = new int[4];
+    public static void main0(String[] args) {
+        do {
+            valami();
+        } while ();
     }
 
+    
+    
+    
     private static int feladatOsszeAllit() {
            
     }
 
-    private static void bekerFeladatTipus() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private static int bekerFeladatTipus() {
+        return ;
     }
 
     private static int feladat() {
@@ -34,8 +33,48 @@ public class GyakorloGyarekeknek {
     }
 
     private static void bekerValasz() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+    }
+
+    private static void valami() {
+       int feladatTipus = bekerFeladatTipus();
+        do {
+           int megoldas = feladatOsszeAllit();
+           
+           
+        } while (beAkarMeg());
+    }
+
+    private static boolean beAkarMeg() {
+        Scanner scan = new Scanner(System.in);
+        boolean jo = false;
+        boolean tovabb = false;
+        String valasz;
+        do {
+            System.out.println("Akar még feladatot? Igen:I || Nem:N");
+            valasz = scan.nextLine();
+            if (valasz.equals("I")) {
+                tovabb = true;
+                jo = true;
+            }else if(valasz.equals("N")){
+                jo = true;
+                tovabb = false;
+            }else{
+                System.out.println("Rossz válasz! Adja meg újra!");
+            }
+            
+        } while (!jo);
+        return tovabb;
     }
     
+    public static void main(String[] args) {
+          Random rnd = new Random();
+            
+            for (int i = 0; i < 100; i++) {
+                int szam = rnd.nextInt(100-1+1)+1;
+                System.out.println(szam);
+            
+        }
+    }
     
-}
+            }
