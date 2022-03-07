dc814949c685c73804986c33efb59b9f4e563e1b
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-03-07 22:36:16 +0100

    Muvelet ellenorzes

--- gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
+++ gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
@@ -1,11 +1,68 @@
 package gyakorlogyarekeknek;
 
+import java.util.Random;
+
 
 public class GyakorloGyarekeknek {
     
     
     public static void main(String[] args) {
-       System.out.println("Szia");
+       osszeadasEllenorzes(7,95);
+       szorzatEllenorzes(25,20);
+       kivonasEllenorzes(30,40);
+       osztasEllenorzes(70,25);
+    }
+
+    private static boolean osszeadasEllenorzes(int szam1, int szam2) {
+       Random rnd = new Random();
+       int szamlalao = 0;
+       while((szam1 + szam2) > 100){
+         szam1 = rnd.nextInt(99 - 1)+ 1;
+         szam2 = rnd.nextInt(99 - 1)+ 1;
+         szamlalao++;
+     }
+        System.out.println(szam1+"+"+szam2+" = "+(szam1+szam2)+" Ennyiszer generált újjat: "+szamlalao);
+       return true;
+     
+    }
+
+    private static boolean szorzatEllenorzes(int szam1, int szam2) {
+        Random rnd = new Random();
+        int szamlalao = 0;
+       while((szam1 * szam2) > 100){
+        szam1 = rnd.nextInt(99 - 1)+ 1;
+        szam2 = rnd.nextInt(99 - 1)+ 1;
+        szamlalao++;
+     }
+       System.out.println(szam1+"*"+szam2+" = "+(szam1*szam2)+" Ennyiszer generált újjat: "+szamlalao);
+       return true;
+
+    }
+
+    private static boolean kivonasEllenorzes(int szam1, int szam2) {
+        Random rnd = new Random();
+        int szamlalao = 0;
+        while((szam1 - szam2) <= 0){
+            szam1 = rnd.nextInt(99 - 1)+ 1;
+            szam2 = rnd.nextInt(99 - 1)+ 1;
+            szamlalao++;
+     }
+        System.out.println(szam1+"-"+szam2+" = "+(szam1-szam2)+" Ennyiszer generált újjat: "+szamlalao);
+        return false;
+
+    }
+
+    private static boolean osztasEllenorzes(int szam1, int szam2) {
+        Random rnd = new Random();
+        int szamlalao = 0;
+        while(szam1 % szam2 != 0){
+            szam1 = rnd.nextInt(99 - 1)+ 1;
+            szam2 = rnd.nextInt(99 - 1)+ 1;
+            szamlalao++;
+     }
+        System.out.println(szam1+"/"+szam2+" = "+(szam1/szam2)+" Ennyiszer generált újjat: "+szamlalao);
+        return false;
+
     }
     
 }
