0ba87d4daab7e5a15012c87663c2078a17a08d55
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-08 20:24:33 +0100

    Random számok egy metódusban

--- gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
+++ gyakorloGyarekeknek/src/gyakorlogyarekeknek/GyakorloGyarekeknek.java
@@ -1,105 +1,102 @@
 package gyakorlogyarekeknek;
 
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import static java.util.Comparator.reverseOrder;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GyakorloGyarekeknek {
+
     //static változók
     static char[] operatorok = {'+', '-', '*', '/'};
     static int[] rosszMegoldasok = new int[4];
-    static int[] hanyDBfeladat = new int[4];   
-   
-    public static void main0(String[] args) {
+    static int[] hanyDBfeladat = new int[4];
+
+    public static void main(String[] args) {
         do {
             torzs();
         } while (beAkarMeg("Akar más féle feladatokat megoldani? Igen:I || Nem:N"));//rossz
         statisztika();
     }
-    public static void main(String[] args) {
+
+
+
+    private static boolean osszeadasEllenorzes(int szam1, int szam2) { // nincs használva
         Random rnd = new Random();
-        int szam1 = rnd.nextInt(99)+1;
-        for (int i = 0; i < 1000; i++) {
-            szam1 = rnd.nextInt(99)+1;
-            System.out.println(szam1);
+        while ((szam1 + szam2) > 100) {
+            szam1 = rnd.nextInt(99) + 1;
+            szam2 = rnd.nextInt(99) + 1;
         }
-                    
+        return true;
+
     }
 
-    private static boolean osszeadasEllenorzes(int szam1, int szam2) {
-       Random rnd = new Random();
-       while((szam1 + szam2) > 100){
-         szam1 = rnd.nextInt(99)+ 1;
-         szam2 = rnd.nextInt(99)+ 1;
-     }
-       return true;
-     
-    }
-
-    private static boolean szorzatEllenorzes(int szam1, int szam2) {
+    private static boolean szorzatEllenorzes(int szam1, int szam2) { // nincs használva
         Random rnd = new Random();
-       while((szam1 * szam2) > 100){
-        szam1 = rnd.nextInt(99)+ 1;
-        szam2 = rnd.nextInt(99)+ 1;
-     }
-       return true;
+        while ((szam1 * szam2) > 100) {
+            szam1 = rnd.nextInt(99) + 1;
+            szam2 = rnd.nextInt(99) + 1;
+        }
+        return true;
 
     }
 
-    private static boolean kivonasEllenorzes(int szam1, int szam2) {
+    private static boolean kivonasEllenorzes(int szam1, int szam2) { // nincs használva
         Random rnd = new Random();
-        while((szam1 - szam2) <= 0){
-            szam1 = rnd.nextInt(99)+ 1;
-            szam2 = rnd.nextInt(99)+ 1;
-     }
+        while ((szam1 - szam2) <= 0) {
+            szam1 = rnd.nextInt(99) + 1;
+            szam2 = rnd.nextInt(99) + 1;
+        }
         return true;
     }
 
-    private static boolean osztasEllenorzes(int szam1, int szam2) {
+    private static boolean osztasEllenorzes(int szam1, int szam2) { // nincs használva
         Random rnd = new Random();
-        while(szam1 % szam2 != 0){
-            szam1 = rnd.nextInt(99)+ 1;
-            szam2 = rnd.nextInt(99)+ 1;
-     }
+        while (szam1 % szam2 != 0) {
+            szam1 = rnd.nextInt(99) + 1;
+            szam2 = rnd.nextInt(99) + 1;
+        }
         return true;
-        
+
     }
 
     private static String ellenorzesInt(String valasz) {
         Scanner scr = new Scanner(System.in);
-        boolean isNumeric = valasz.chars().allMatch( Character::isDigit );
-        while(!isNumeric){
+        boolean isNumeric = valasz.chars().allMatch(Character::isDigit);
+        while (!isNumeric) {
             kiiras("Kérem számot adjon meg: ");
             //System.out.println("Kérem számot adjon meg: ");
-            valasz =scr.nextLine();
-            isNumeric = valasz.chars().allMatch( Character::isDigit );
+            valasz = scr.nextLine();
+            isNumeric = valasz.chars().allMatch(Character::isDigit);
         }
         return valasz;
     }
 
-
     private static int feladatOsszeAllit(char feladatTipusa) {
-        
-           int szam1 = rndszamGeneralas(feladatTipusa);
-           int szam2 = rndszamGeneralas(feladatTipusa);
-           int vegeredmeny=0;
-           
-           if (feladatTipusa =='+') {
-               kiiras(szam1+"+"+szam2);
-               vegeredmeny=szam1+szam2;
-           } else if (feladatTipusa=='-'){
-               kiiras(szam1+"-"+szam2);
-                vegeredmeny=szam1-szam2;
-           } else if (feladatTipusa=='*'){
-               kiiras(szam1+"*"+szam2);
-                vegeredmeny=szam1*szam2;
-           } else if (feladatTipusa=='/'){
-               kiiras(szam1+"/"+szam2);
-                vegeredmeny=szam1/szam2;
-           }
-           /* 
+
+//           int szam1 = rndszamGeneralas(feladatTipusa);
+//           int szam2 = rndszamGeneralas(feladatTipusa);
+        int[] szamTomb = rndszamGeneralas(feladatTipusa);
+        int szam1 = szamTomb[0], szam2 = szamTomb[1];
+        int vegeredmeny = 0;
+
+        if (feladatTipusa == '+') {
+            kiiras(szam1 + "+" + szam2);
+            vegeredmeny = szam1 + szam2;
+        } else if (feladatTipusa == '-') {
+            kiiras(szam1 + "-" + szam2);
+            vegeredmeny = szam1 - szam2;
+        } else if (feladatTipusa == '*') {
+            kiiras(szam1 + "*" + szam2);
+            vegeredmeny = szam1 * szam2;
+        } else if (feladatTipusa == '/') {
+            kiiras(szam1 + "/" + szam2);
+            vegeredmeny = szam1 / szam2;
+        }
+        /* 
            
         switch (bekerFeladatTipus()) {
             case bekerFeladatTipus()=='+':
@@ -115,9 +112,9 @@ public class GyakorloGyarekeknek {
                 kiiras(szam1+"*"+szam2);
                 vegeredmeny=szam1*szam2;
         }
-           */
-           return vegeredmeny;
-        }  
+         */
+        return vegeredmeny;
+    }
 
     private static char bekerFeladatTipus() {
         //System.out.println("Kéremd adja meg milyen alapműveletet szeretne[+ - * /]");
@@ -130,33 +127,34 @@ public class GyakorloGyarekeknek {
             muvjel = sc.next().charAt(0);
         }
         return muvjel;
-    } 
-    private static int bekerValasz() {
+    }
+
+    private static int bekerValasz() { //try catchre mert evvel lehet hibára futni
         Scanner scr = new Scanner(System.in);
-        kiiras("Kérem adja meg a megoldást: ");
+        kiiras("Kérem adja meg a megoldást: "); 
         //System.out.println("Kérem adja meg a megoldást: ");
-        String valasz =scr.nextLine();
-        valasz =ellenorzesInt(valasz);
+        String valasz = scr.nextLine();
+        valasz = ellenorzesInt(valasz);
         int megoldas = Integer.parseInt(valasz);
         return megoldas;
     }
 
     private static void torzs() {
-       char feladatTipus = bekerFeladatTipus();
-       int hely = opHelye(feladatTipus);
+        char feladatTipus = bekerFeladatTipus();
+        int hely = opHelye(feladatTipus);
         do {
-           int megoldas = feladatOsszeAllit(feladatTipus);
-           hanyDBfeladat[hely] += 1;
-           int felhaszValasz = bekerValasz();
-           if(felhaszValasz != megoldas){
-               rosszMegoldasok[hely] += 1;
-               kiiras("Rossz megoldást adott meg!");
-               //System.out.println("Rossz megoldást adott meg!");
-           }else{
-               kiiras("jó megoldást adott meg!");
-               //System.out.println("jó megoldást adott megg!");
-           }
-           
+            int megoldas = feladatOsszeAllit(feladatTipus);
+            hanyDBfeladat[hely] += 1;
+            int felhaszValasz = bekerValasz();
+            if (felhaszValasz != megoldas) {
+                rosszMegoldasok[hely] += 1;
+                kiiras("Rossz megoldást adott meg!");
+                //System.out.println("Rossz megoldást adott meg!");
+            } else {
+                kiiras("jó megoldást adott meg!");
+                //System.out.println("jó megoldást adott megg!");
+            }
+
         } while (beAkarMeg("Akar még feladatot? Igen:I || Nem:N"));
     }
 
@@ -172,48 +170,54 @@ public class GyakorloGyarekeknek {
             if (valasz.equals("i")) {
                 tovabb = true;
                 jo = true;
-            }else if(valasz.equals("n")){
+            } else if (valasz.equals("n")) {
                 jo = true;
                 tovabb = false;
-            }else{
+            } else {
                 kiiras("Rossz adat! Adja meg újra!");
                 //System.out.println("Rossz adat! Adja meg újra!");
             }
-            
+
         } while (!jo);
         return tovabb;
     }
+
     private static int opHelye(char feladatTipus) {
         int helye;
-        int hossz=operatorok.length;
+        int hossz = operatorok.length;
         int i = 0;
         while (!(feladatTipus == operatorok[i])) {
             i++;
         }
         return i;
     }
-    private static int[] rndszamGeneralas(char feladatTipus) {
-       Random rnd = new Random();
-       int felso = 99;
-       int also = 1;
-       int[] tomb = new int[2];
-       if(feladatTipus == '+'){
-           tomb[0] = rnd.nextInt(felso-also+1)+also;
-           tomb[1] = rnd.nextInt(felso-tomb[0]+1)+1;
-       }else if(feladatTipus == '-'){
-           tomb[0] = rnd.nextInt(felso-also+1)+also;
-           tomb[1] = rnd.nextInt(felso-tomb[0]+1);//array sortot majd ki kell adni
-       }else if(feladatTipus == '*'){
-           tomb[0] = rnd.nextInt(felso-also+1)+also;
-           felso = (int) Math.floor(felso/tomb[0]);
-           tomb[1] = rnd.nextInt(felso-also+1);
-       }else{
-           tomb[0] = rnd.nextInt(felso-also+1)+also;
-           felso = tomb[0];
-       }                  
-       return tomb;
 
-}
+    private static int[] rndszamGeneralas(char feladatTipus) {
+        Random rnd = new Random();
+        int felso = 99;
+        int also = 1;
+        //Integer[] tomb = new Integer[2];
+        int[] tomb = new int[2];
+        if (feladatTipus == '+') {
+            tomb[0] = rnd.nextInt(felso - also + 1) + also;
+            tomb[1] = rnd.nextInt(felso - tomb[0] + 1) + 1;
+        } else if (feladatTipus == '-') {
+            tomb[0] = rnd.nextInt(felso - also + 1) + also;
+            tomb[1] = rnd.nextInt(tomb[0])+1; //array sortot majd ki kell adni
+            //Arrays.sort(tomb, Comparator.reverseOrder()); // még ez sem az igazi
+        } else if (feladatTipus == '*') {
+            tomb[0] = rnd.nextInt(felso - also + 1) + also;
+            felso = (int) Math.floor(felso / tomb[0]);
+            tomb[1] = rnd.nextInt(felso) + 1;
+        } else {
+            tomb[0] = rnd.nextInt(felso - also + 1) + also;
+            ArrayList<Integer> osztok = szamOsztoi(tomb[0]);
+            tomb[1] = osztok.get(rnd.nextInt(osztok.size()));
+        }
+        return tomb;
+
+    }
+
     static void statisztika() {
         int legtobbHiba = maxErtek();
         System.out.println("123");
@@ -231,9 +235,19 @@ public class GyakorloGyarekeknek {
         }
         return legNagyHely;
     }
+
     private static void kiiras(String szoveg) {
         System.out.println(szoveg);
     }
-    
+
+    private static ArrayList<Integer> szamOsztoi(int szam) {
+        ArrayList<Integer> osztok = new ArrayList<>();
+        for (int i = 1; i <= szam; i++) {
+            if (szam % i == 0) {
+                osztok.add(i);
+            }
+        }
+        return osztok;
+    }
 
 }
---
+++ gyakorloGyarekeknek/src/gyakorlogyarekeknek/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gyakorlogyarekeknek;
+
+/**
+ *
+ * @author Benedek
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        int szam = 50;
+        System.out.println(50 % 25 == 0);
+    }
+}
